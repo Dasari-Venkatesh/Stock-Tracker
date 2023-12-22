b2eb23a02762debd9ca550dbf6a00ebcf5b9c16c
Dasari-Venkatesh <venkateshdasari021@gmail.com>
2023-12-22 19:39:41 +0530

    Add: added stocktracker view

--- api/viewer/views.py
+++ api/viewer/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def stockPicker(request):
     
-    return HttpResponse("Hey I am picking stocks")
+    # return HttpResponse("Hey I am picking stocks")
+    return render(request, 'viewer/stockpicker.html')
+
+def stockTracker(request):
+
+    return render(request,'viewer/stocktracker.html')
