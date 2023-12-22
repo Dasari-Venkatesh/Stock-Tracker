7a84958a4a4e17e1915c7b717a55529930b243d2
Dasari-Venkatesh <venkateshdasari021@gmail.com>
2023-12-22 18:59:35 +0530

    Added basic templates

--- api/viewer/views.py
+++ api/viewer/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def stockPicker(request):
+    
+    return HttpResponse("Hey I am picking stocks")
